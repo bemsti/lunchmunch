0744938f550ed965ab64725f6298fec146393bc4
Colin Agbabiaka <colina@colina-VirtualBox.(none)>
2013-04-28 01:33:13 +0100

    user template - home page

---
+++ code/mojits/meal/models/foo.server.js
@@ -0,0 +1,34 @@
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('mealModelFoo', function(Y, NAME) {
+
+/**
+ * The mealModelFoo module.
+ *
+ * @module meal
+ */
+
+    /**
+     * Constructor for the mealModelFoo class.
+     *
+     * @class mealModelFoo
+     * @constructor
+     */
+    Y.namespace('mojito.models')[NAME] = {
+
+        init: function(config) {
+            this.config = config;
+        },
+
+        /**
+         * Method that will be invoked by the mojit controller to obtain data.
+         *
+         * @param callback {function(err,data)} The callback function to call when the
+         *        data has been retrieved.
+         */
+        getData: function(callback) {
+            callback(null, { some: 'data' });
+        }
+
+    };
+
+}, '0.0.1', {requires: []});
